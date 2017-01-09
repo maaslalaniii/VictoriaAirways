607ed38fad5d3f8ea994cbd9d63fce74a2aa8172
Maas Lalani <maas@live.ca>
2017-01-09 14:56:53 -0500

    Create Flight.java

---
+++ Flight.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+
+/**
+ * A flight of the airline.
+ * 
+ * @author Maas Lalani, Jenisha Thomas, Ming Zhao Huang
+ * @version 1.0
+ */
+public class Flight
+{
+    /* instance fields */
+    Cargo[] cargo;
+    double cost;
+    Date date;
+    String destination;
+    String departure;
+    Passenger[] passenger;
+    Plane plane;
+    
+    /**
+     * Constructor for objects of class Flight
+     */
+    public Flight()
+    {
+    } // end of constructor Flight()
+} // end of class Flight
