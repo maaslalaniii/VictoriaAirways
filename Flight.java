74634f5249045996aed37fb49b2d9ce166515e66
saintsdrewbrees <sam.mingzhao.h@gmail.com>
2017-01-16 11:10:31 -0500

    Changed comments

--- Flight.java
+++ Flight.java
@@ -1,10 +1,10 @@
 import java.util.Date;
 
 /**
- * A flight
+ * A flight which journeys from a point of departure to a destination.
  * 
  * @author Maas Lalani, Jenisha Thomas, Ming Zhao Huang
- * @version 1.0
+ * @version 1.0 2016-12-23
  */
 public class Flight
 {
@@ -12,11 +12,12 @@ public class Flight
     private Cargo[] cargo;
     private double cost;
     private Date date;
-    private String destination;
     private String departure;
+    private String destination;
     private Passenger[] passenger;
     private Plane plane;
 
+    /* constructors */
     /**
      * Constructs a flight with the specified characteristics.
      * 
@@ -40,9 +41,106 @@ public class Flight
         this.departure = departure;
         this.passenger = new Passenger[plane.getMaximumNumberOfPassengers()];
         this.plane = plane;
-    } // end of constructor Flight()
+    } // end of constructor Flight(double cost..)
+    
+    /* accessors */
+    /**
+     * Returns the cargo of this flight.
+     * 
+     * @return the cargo of this flight
+     */
+    public Cargo[] getCargo()
+    {
+        return this.cargo;
+    } // end of method getCargo()
 
-    /* cargo */
+    /**
+     * Returns the cost of a single ticket of this flight.
+     * 
+     * @return the cost of a single ticket of this flight
+     */
+    public double getCost()
+    { 
+        return this.cost;
+    } // end method getCost()
+    
+    /**
+     * Returns the date of this flight.
+     *
+     * @return the date of this flight
+     */
+    public Date getDate()
+    {
+        return this.date;
+    } // end method getDate()
+
+    /**
+     * Returns the departure of this flight.
+     *
+     * @return the departure of this flight
+     */
+    public String getDeparture()
+    {
+        return this.departure;
+    } // end method getDeparture()
+
+    /**
+     * Returns the destination of this flight.
+     *
+     * @return the destination of this flight
+     */
+    public String getDestination()
+    {
+        return this.destination;
+    } // end method getDestination()
+    
+    /**
+     * Returns the plane of this flight.
+     *
+     * @return the plane of this flight
+     */
+    public Plane getPlane()
+    {
+        return this.plane;
+    } // end method getPlane()
+    
+    /**
+     * Returns the passengers of this flight.
+     *
+     * @return the passengers of this flight
+     */
+    public Passenger[] getPassengers()
+    {
+        return this.passenger;
+    } // end method getPassengers()
+    
+    /* mutators */
+    /**
+     * Sets the plane of this flight.
+     *
+     * @param plane the plane of this flight
+     */
+    public void setPlane(Plane plane)
+    {
+        if (plane == null) return;
+
+        this.plane = plane;
+    } // end method setPlane(Plane plane)
+    
+    /**
+     * Sets the passengers of this flight.
+     *
+     * @param passenger the passengers of this flight
+     */
+    public void setPassengers(Passenger[] passenger)
+    {
+        if (passenger == null) return;
+        if (this.passenger.length 
+			> plane.getMaximumNumberOfPassengers()) return;
+
+        this.passenger = passenger;
+    } // end method setPassengers(Passenger[] passenger)
+
     /**
      * Sets the cargo of this flight.
      * 
@@ -54,18 +152,8 @@ public class Flight
         if (cargo.length > plane.getMaximumNumberOfItemsOfCargo()) return;
 
         this.cargo = cargo;
-    } // end method setCargo()
-
-    /**
-     * Returns the cargo of this flight.
-     * 
-     * @return the cargo of this flight
-     */
-    public Cargo[] getCargo()
-    {
-        return this.cargo;
-    } // end of method getCargo()
-
+    } // end method setCargo(Cargo[] cargo)
+    
     /**
      * Adds an item of cargo to the flight's cargo.
      * 
@@ -81,8 +169,7 @@ public class Flight
 
         this.cargo[this.cargo.length] = cargo;
     } // end of method addCargo(Cargo cargo)
-
-    /* cost */
+    
     /**
      * Sets the cost of a single ticket of this flight.
      *
@@ -93,19 +180,8 @@ public class Flight
         if (cost < 0) return;
 
         this.cost = cost;
-    } // end method setCost()
-
-    /**
-     * Returns the cost of a single ticket of this flight.
-     * 
-     * @return the cost of a single ticket of this flight
-     */
-    public double getCost()
-    { 
-        return this.cost;
-    } // end method getCost()
-
-    /* date */
+    } // end method setCost(double cost)
+    
     /**
      * Sets the date of this flight.
      *
@@ -116,19 +192,8 @@ public class Flight
         if (date == null) return;
 
         this.date = date;
-    } // end method setDate()
-
-    /**
-     * Returns the date of this flight.
-     *
-     * @return the date of this flight
-     */
-    public Date getDate()
-    {
-        return this.date;
-    } // end method getDate()
-
-    /* destination */
+    } // end method setDate(Date date)
+    
     /**
      * Sets the destination of this flight.
      *
@@ -139,20 +204,9 @@ public class Flight
         if (destination == null) return;
 
         this.destination = destination;
-    } // end method setDestination()
-
-    /**
-     * Returns the destination of this flight.
-     *
-     * @return the destination of this flight
-     */
-    public String getDestination()
-    {
-        return this.destination;
-    } // end method getDestination()
-
-    /* departure */
-    /**
+    } // end method setDestination(String destination)
+    
+     /**
      * Sets the departure of this flight.
      *
      * @param departure the departure of this flight
@@ -162,64 +216,5 @@ public class Flight
         if (departure == null) return;
 
         this.departure = departure;
-    } // end method setDeparture()
-
-    /**
-     * Returns the departure of this flight.
-     *
-     * @return the departure of this flight
-     */
-    public String getDeparture()
-    {
-        return this.departure;
-    } // end method getDeparture()
-
-    /* passengers */
-    /**
-     * Sets the passengers of this flight.
-     *
-     * @param passenger the passengers of this flight
-     */
-    public void setPassengers(Passenger[] passenger)
-    {
-        if (passenger == null) return;
-        if (this.passenger.length 
-			> plane.getMaximumNumberOfPassengers()) return;
-
-        this.passenger = passenger;
-    } // end method setPassengers()
-
-    /**
-     * Returns the passengers of this flight.
-     *
-     * @return the passengers of this flight
-     */
-    public Passenger[] getPassengers()
-    {
-        return this.passenger;
-    } // end method getPassengers()
-	
-    /* plane */
-    /**
-     * Sets the plane of this flight.
-     *
-     * @param plane the plane of this flight
-     */
-    public void setPlane(Plane plane)
-    {
-        if (plane == null) return;
-
-        this.plane = plane;
-    } // end method setPlane()
-
-    /**
-     * Returns the plane of this flight.
-     *
-     * @return the plane of this flight
-     */
-    public Plane getPlane()
-    {
-        return this.plane;
-    } // end method getPlane()
-
+    } // end method setDeparture(String departure)
 } // end of class Flight
