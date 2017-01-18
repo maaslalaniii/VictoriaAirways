baa8041b880e444f902bc8624a6f2c4e63d1f466
saintsdrewbrees <sam.mingzhao.h@gmail.com>
2017-01-18 09:30:19 -0500

    Fixed comments

--- Flight.java
+++ Flight.java
@@ -17,8 +17,6 @@ public class Flight
     private Passenger[] passenger;
     private Plane plane;
 
-
-
     /* constructors */
     /**
      * Constructs a flight with the specified characteristics.
