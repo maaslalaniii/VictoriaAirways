e5a2707baed36a90f29a01af0b6ee205ec80e2a8
saintsdrewbrees <sam.mingzhao.h@gmail.com>
2017-01-18 09:10:07 -0500

    Fixed comments

--- Boeing737Medium.java
+++ Boeing737Medium.java
@@ -2,21 +2,22 @@
  * A medium range Boeing plane.
  * 
  * @author Maas Lalani, Jenisha Thomas, Ming Zhao Huang 
- * @version 1.0 2017-1-11
+ * @version 1.0 2017-01-11
  */
 public class Boeing737Medium extends Plane
+{
+    /* constructors */
+    /**
+     * Constructs a plane with the characteristics
+     * of a Boeing 737 Medium range aircraft.
+     * 
+     * @param name the name of this Boeing 737
+     * Medium plane
+     * @param location the location of this Boeing 737
+     * Medium plane 
+     */
+    public Boeing737Medium(String name, String location)
     {
-        /**
-         * Constructs a plane with the characteristics
-         * of a Boeing 737 Medium range aircraft.
-         * 
-         * @param name the name of this Boeing 737
-         * Medium plane
-         * @param location the location of this Boeing 737
-         * Medium plane 
-         */
-        public Boeing737Medium(String name, String location)
-        {
-            super(name, 140, "Boeing 737", 20, 6, false, "Medium", location);
-        }// end of contructor Boeing737Medium(String name, String location)
-    }// end of class BoeingShort
+        super(name, 140, "Boeing 737", 20, 6, false, "Medium", location);
+    } // end of contructor Boeing737Medium(String name, String location)
+} // end of class BoeingShort
