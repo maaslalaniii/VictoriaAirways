48b4df2d319b18a6f89501702af03703aca3a8ff
saintsdrewbrees <sam.mingzhao.h@gmail.com>
2017-01-18 09:06:32 -0500

    Fixed comments

--- AirbusA340.java
+++ AirbusA340.java
@@ -5,19 +5,19 @@
  * @version 1.0 2017-1-11
  */
 public class AirbusA340 extends Plane
+{
+    /* constructors */
+    /**
+     * Constructs a plane with the characteristics
+     * of an Airbus A340 Long range aircraft.
+     * 
+     * @param name the name of this Airbus A340 Long 
+     * range plane
+     * @param location the location of this Airbus A340 
+     * Long range plane 
+     */
+    public AirbusA340(String name, String location)
     {
-        /* constructors */
-        /**
-         * Constructs a plane with the characteristics
-         * of an Airbus A340 Long range aircraft.
-         * 
-         * @param name the name of this Airbus A340 Long 
-         * range plane
-         * @param location the location of this Airbus A340 
-         * Long range plane 
-         */
-        public AirbusA340(String name, String location)
-        {
-            super(name, 200, "Airbus A340", 30, 6, false, "Long", location);
-        } // end of contructor AirbusA340(String name, String location)
-    } // end of class AirbusA340
+        super(name, 200, "Airbus A340", 30, 6, false, "Long", location);
+    } // end of contructor AirbusA340(String name, String location)
+} // end of class AirbusA340
