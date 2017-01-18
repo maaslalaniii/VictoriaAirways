c00fb0f13333f047a32acfd151050c40a1fc9cbd
saintsdrewbrees <sam.mingzhao.h@gmail.com>
2017-01-18 09:11:20 -0500

    Fixed comments

--- Boeing737Short.java
+++ Boeing737Short.java
@@ -2,21 +2,22 @@
  * A short range Boeing plane.
  * 
  * @author Maas Lalani, Jenisha Thomas, Ming Zhao Huang 
- * @version 1.0 2017-1-11
+ * @version 1.0 2017-01-11
  */
 public class Boeing737Short extends Plane
+{
+    /* constructors */
+    /**
+     * Constructs a plane with the characteristics
+     * of a Boeing 737 Short range aircraft.
+     * 
+     * @param name the name of this Boeing 737
+     * Short plane
+     * @param location the location of this Boeing 737
+     * Short plane 
+     */
+    public Boeing737Short(String name, String location)
     {
-       /**
-         * Constructs a plane with the characteristics
-         * of a Boeing 737 Short range aircraft.
-         * 
-         * @param name the name of this Boeing 737
-         * Short plane
-         * @param location the location of this Boeing 737
-         * Short plane 
-         */
-        public Boeing737Short(String name, String location)
-        {
-            super(name, 80, "Boeing 737", 10, 6, false, "Short", location);
-        }// end of contructor Boeing737Short(String name, String location)
-    }// end of class BoeingShort
+        super(name, 80, "Boeing 737", 10, 6, false, "Short", location);
+    } // end of contructor Boeing737Short(String name, String location)
+} // end of class Boeing737Short
