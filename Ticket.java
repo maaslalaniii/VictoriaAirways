5a3432f726d02f968b5cabb0c20b9fd2914281cb
saintsdrewbrees <sam.mingzhao.h@gmail.com>
2017-01-19 14:00:26 -0500

    Fixed comments

--- Ticket.java
+++ Ticket.java
@@ -22,8 +22,7 @@ public class Ticket
      * <br><i>pre-condition </i>reservedSeat may not 
      * be <code>null</code>
      */
-    public Ticket(Flight reservedFlight, 
-				  String reservedSeat)
+    public Ticket(Flight reservedFlight, String reservedSeat)
     {
         // Check validity of parameters.
         if (reservedFlight == null) return;
@@ -31,7 +30,6 @@ public class Ticket
 
         this.reservedFlight = reservedFlight;
         this.reservedSeat = reservedSeat;
-
     } // end of constructor Ticket(Flight reservedFlight...)
 
     /* accessors */
@@ -54,7 +52,6 @@ public class Ticket
     {
         return this.reservedSeat;
     } // end of method getReservedSeat()
-
 
     /**
      * Returns a string representation of this ticket.
@@ -99,5 +96,4 @@ public class Ticket
         if (reservedSeat == null) return;
         this.reservedSeat = reservedSeat;
     } // end of method setReservedSeat(Seat reservedSeat)
-
 } // end of class Ticket
